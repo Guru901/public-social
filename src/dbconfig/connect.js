4db5ccb683ef82a58330eadb1026978ddc8483a1
Gurvinder Singh <133991448+Guru901@users.noreply.github.com>
2024-02-25 18:12:44 +0530

    Update connect.js

--- src/dbconfig/connect.js
+++ src/dbconfig/connect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connect = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://avinashkaur842:t9Mz1bJq3HgsaVhL@cluster0.6hic2ku.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://avinashkaur542:kr$signOP699090@cluster0.781kpom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     );
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
